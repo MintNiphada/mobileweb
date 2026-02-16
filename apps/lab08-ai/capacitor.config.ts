c195f8fca96f28853c44473b649d90e115a0c037
MintNiphada <niphada.y@kkumail.com>
2026-02-16 16:01:34 +0700

    lab8 first

---
+++ apps/lab08-ai/capacitor.config.ts
@@ -0,0 +1,11 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+
+const config: CapacitorConfig = {
+  appId: 'cpkku.niphada.lab08',
+  appName: 'lab08-ai',
+  webDir: 'dist'
+};
+
+
+export default config;
---
+++ apps/lab08-ai/src/core/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_A6f4LIw3Uc5jyOxOxU7bdknm-nJSD5Y",
+  authDomain: "web2025-mobilewebapp.firebaseapp.com",
+  projectId: "web2025-mobilewebapp",
+  storageBucket: "web2025-mobilewebapp.firebasestorage.app",
+  messagingSenderId: "702141441945",
+  appId: "1:702141441945:web:5275319138767af2045508",
+  measurementId: "G-7TW3VETC0T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
